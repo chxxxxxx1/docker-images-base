285b8507b860a8102b623a4473d105af97a727aa
chessman <cjb15770595346@163.com>
2024-06-03 21:34:11 +0800

    feat: 添加zx脚本构建🚀

---
+++ nginx/build.mjs
@@ -0,0 +1,9 @@
+#!/usr/bin/env zx
+
+import fse from "fs-extra";
+const pkg = fse.readJSONSync("./package.json");
+await $`docker buildx build --platform linux/amd64,linux/arm64 -t registry.cn-hangzhou.aliyuncs.com/images-lib/image-base:nginx-${pkg.version} --push .`;
+
+console.log(
+  `镜像 registry.cn-hangzhou.aliyuncs.com/images-lib/image-base:nginx-${pkg.version} 已经推送完毕～`,
+);
